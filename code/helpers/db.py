34b144de367edb2717dc6beec9b879f33158a475
Rien Boydens <rien.boydens@gmail.com>
2023-04-29 12:45:18 +0200

    Relatie wind en golven

--- code/helpers/db.py
+++ code/helpers/db.py
@@ -5,7 +5,9 @@ Defines a simple decorator to provide a function with a database connection. Aft
 '''
 from .config import dbname, user, host, password, gssencmode
 from psycopg2 import connect
+import psycopg2.extensions
 from functools import wraps
+import datetime
 
 def db_connection(f):
     @wraps(f)
@@ -16,3 +18,12 @@ def db_connection(f):
         conn.close()
         return retval
     return wrapped_f
+
+
+class CSVCursor(psycopg2.extensions.cursor):
+    def __str__(self):
+        newline = '\n'
+        return f"""\
+{ ','.join([a.name for a in self.description]) }
+{ newline.join([','.join([item.isoformat() if type(item) == datetime.datetime else str(item) for item in row]) for row in self]) }
+"""
--- code/server.py
+++ code/server.py
@@ -4,11 +4,12 @@ Provides the api routes.
 More details.
 """
 
-from flask import Flask, send_from_directory, request
+from flask import Flask, send_from_directory, request, Response
 from flask_compress import Compress
 from flask_cors import CORS, cross_origin
 from flask_expects_json import expects_json
-from .helpers.db import db_connection
+from .helpers.db import db_connection, CSVCursor
+import datetime
 
 app = Flask(__name__)
 compress = Compress()
@@ -65,6 +66,38 @@ def beschikbaarheden(conn=None):
     return (years, 200) if years else ({}, 500)
 
 
+@app.route('/api/windsnelheid')
+@db_connection
+def windsnelheid(conn=None):
+    min_date = request.args.get('min_date', type=datetime.datetime.fromisoformat)
+    max_date = request.args.get('max_date', type=datetime.datetime.fromisoformat)
+    if min_date is None or max_date is None:
+        return Response(status=400)
+
+    with conn.cursor(cursor_factory=CSVCursor) as cur:
+        cur.execute("""
+            SELECT datum, value FROM measurements m INNER JOIN lookup l ON m.series_id=l.series_id 
+            WHERE l.par='WVS' AND datum >= %(min_date)s AND datum <= %(max_date)s ORDER BY datum;
+        """, { 'min_date': min_date, 'max_date': max_date })
+        return str(cur)
+
+
+@app.route('/api/golfhoogte')
+@db_connection
+def golfhoogte(conn=None):
+    min_date = request.args.get('min_date', type=datetime.datetime.fromisoformat)
+    max_date = request.args.get('max_date', type=datetime.datetime.fromisoformat)
+    if min_date is None or max_date is None:
+        return Response(status=400)
+    
+    with conn.cursor(cursor_factory=CSVCursor) as cur:
+        cur.execute("""
+            SELECT datum, value FROM measurements m INNER JOIN lookup l ON m.series_id=l.series_id 
+            WHERE l.par='GHA' AND datum >= %(min_date)s AND datum <= %(max_date)s ORDER BY datum;
+        """, { 'min_date': min_date, 'max_date': max_date })
+        return str(cur)
+
+
 @app.route('/beschikbaarheden')
 def beschikbaarhedenview():
     """!
@@ -74,10 +107,10 @@ def beschikbaarhedenview():
     return send_from_directory('files',"beschikbaarheden.html")
 
 
-@app.route('/static/<path:path>')
+@app.route('/<path:path>')
 def doc_files(path):
     """!
     Insert new project in management database
     @return a dict with id and names of waterinfo station.
     """
-    return send_from_directory('files/static', path)
+    return send_from_directory('files', path)
